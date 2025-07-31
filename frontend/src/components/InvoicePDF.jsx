663b0a512c68df1ecf162e6da5e63c73527b740d
rahulmishra-01 <rahulmishramail.work@gmail.com>
2025-07-31 17:32:28 +0530

    pdf redesign V2

--- frontend/src/components/InvoicePDF.jsx
+++ frontend/src/components/InvoicePDF.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { toWords } from "number-to-words";
 import {
   Document,
   Page,
@@ -13,7 +14,7 @@ import RobotoBold from "../fonts/Roboto-Bold.ttf";
 
 Font.register({
   family: "Roboto",
-  src:Roboto,
+  src: Roboto,
   fontWeight: "normal",
 });
 
@@ -25,14 +26,182 @@ Font.register({
 
 const styles = StyleSheet.create({
   page: { padding: 30, fontFamily: "Roboto", fontSize: 10 },
-  section: {  marginBottom: 20 , padding: 10, border: "1px solid #ddd", borderRadius: 5 },
-  row: { flexDirection: "row", justifyContent: "space-between", marginBottom: 10, alignItems: "flex-start" ,  flexWrap: "wrap"},
-  bold: { fontWeight: "bold" , fontFamily: "Roboto", fontSize: 10 , marginBottom: 5, textTransform: "uppercase", color: "#333", letterSpacing: 0.5 , marginTop: 5 },
-  heading: { fontSize: 14, fontWeight: "bold", textAlign: "center", textTransform: "uppercase", color: "#333", letterSpacing: 1 , marginTop: 10 , marginBottom: 20 , fontFamily: "Roboto" , borderBottom: "2px solid #ddd", paddingBottom: 5 },
-  tableHeader: { flexDirection: "row", backgroundColor: "#f7f7f7", padding: 5, borderBottom: "2px solid #ddd", marginBottom: 5, fontFamily: "Roboto", fontSize: 10, fontWeight: "bold" },
-  tableCell: { flex: 1, padding: 5, borderBottom: "1px solid #ddd", fontFamily: "Roboto", fontSize: 10 , textAlign: "left" },
-  footerText: { textAlign: "center", fontSize: 8, color: "#777", marginTop: 20, fontFamily: "Roboto", letterSpacing: 0.5, marginBottom: 10 },
-  authorisedSignatory: { textAlign: "right", fontSize: 10, fontWeight: "bold", marginTop: 20, fontFamily: "Roboto", color: "#333", letterSpacing: 0.5, marginBottom: 10 },
+  section: {
+    marginBottom: 20,
+    padding: 10,
+    border: "1px solid #ddd",
+    borderRadius: 5,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
+    alignItems: "flex-start",
+    flexWrap: "wrap",
+  },
+  bold: {
+    fontWeight: "bold",
+    fontFamily: "Roboto",
+    fontSize: 10,
+    marginBottom: 5,
+    textTransform: "uppercase",
+    color: "#333",
+    letterSpacing: 0.5,
+    marginTop: 5,
+  },
+  heading: {
+    fontSize: 14,
+    fontWeight: "bold",
+    textAlign: "center",
+    textTransform: "uppercase",
+    color: "#333",
+    letterSpacing: 1,
+    marginTop: 10,
+    marginBottom: 20,
+    fontFamily: "Roboto",
+    borderBottom: "2px solid #ddd",
+    paddingBottom: 5,
+  },
+  tableHeader: {
+    flexDirection: "row",
+    backgroundColor: "#f7f7f7",
+    padding: 5,
+    borderBottom: "2px solid #ddd",
+    marginBottom: 5,
+    fontFamily: "Roboto",
+    fontSize: 10,
+    fontWeight: "bold",
+  },
+  tableCell: {
+    flex: 1,
+    padding: 5,
+    borderBottom: "1px solid #ddd",
+    fontFamily: "Roboto",
+    fontSize: 10,
+    textAlign: "left",
+  },
+  footerText: {
+    textAlign: "center",
+    fontSize: 8,
+    color: "#777",
+    marginTop: 20,
+    fontFamily: "Roboto",
+    letterSpacing: 0.5,
+    marginBottom: 10,
+  },
+  authorisedSignatory: {
+    textAlign: "right",
+    fontSize: 10,
+    fontWeight: "bold",
+    marginTop: 40,
+    fontFamily: "Roboto",
+    color: "#333",
+    letterSpacing: 0.5,
+    marginBottom: 10,
+  },
+  totalsSection: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    marginTop: 20,
+    padding: 10,
+    border: "1px solid #ddd",
+    borderRadius: 5,
+    backgroundColor: "#f9f9f9",
+    fontFamily: "Roboto",
+    fontSize: 10,
+  },
+  totalInWords: {
+    fontSize: 10,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    color: "#333",
+    letterSpacing: 0.5,
+    marginTop: 5,
+    fontFamily: "Roboto",
+    marginBottom: 10,
+  },
+  totalSection: {
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 10,
+    fontFamily: "Roboto",
+    fontSize: 10,
+    color: "#333",
+    letterSpacing: 0.5,
+    marginBottom: 10,
+    // alignItems: "flex-end",
+  },
+  subTotalSection:{
+    justifyContent: "flex-end",
+  },
+  invoiceDetails:{
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    marginTop: 10,
+    fontFamily: "Roboto",
+    fontSize: 10,
+    color: "#333",
+    letterSpacing: 0.5,
+    marginBottom: 10,
+  },
+  buyerOrderDetails:{
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    marginTop: 10,
+    fontFamily: "Roboto",
+    fontSize: 10,
+    color: "#333",
+    letterSpacing: 0.5,
+    marginBottom: 10
+  },
+  sellerDetails:{
+    width: "50%",
+    fontFamily: "Roboto",
+    fontSize: 10,
+    color: "#333",
+    letterSpacing: 0.5,
+    marginBottom: 10,
+    paddingRight: 20,
+    textTransform: "uppercase",
+  },
+  billToDetails:{
+    fontFamily: "Roboto",
+    fontSize: 10,
+    color: "#333",
+    letterSpacing: 0.5,
+    marginBottom: 10,
+    paddingRight: 20,
+    textTransform: "uppercase",
+  },
+  shipToDetails:{
+    fontFamily: "Roboto",
+    fontSize: 10,
+    color: "#333",
+    letterSpacing: 0.5,
+    marginBottom: 10,
+    textTransform: "uppercase",
+    paddingRight: 20,
+  },
+  buyerSections:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
+    alignItems: "flex-start",
+    flexWrap: "wrap",
+    fontFamily: "Roboto",
+    fontSize: 10,
+    color: "#333",
+    letterSpacing: 0.5,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  Watermark:{
+    marginTop:5,
+  },
 });
 
 const InvoicePDF = ({ invoice }) => {
@@ -43,43 +212,64 @@ const InvoicePDF = ({ invoice }) => {
   const total = invoice.totalAmount || 0;
 
   const taxRate = products.length ? products[0].tax : 0;
-  const subtotal = products.reduce((acc,item) => acc + (item.quantity * item.rate - ((item.discount / 100) * item.quantity * item.rate)), 0);
+  const subtotal = products.reduce(
+    (acc, item) =>
+      acc +
+      (item.quantity * item.rate -
+        (item.discount / 100) * item.quantity * item.rate),
+    0
+  );
   const taxAmount = subtotal * (taxRate / 100);
   return (
-     <Document>
+    <Document>
       <Page style={styles.page}>
         {/* Top Section */}
         <View style={styles.row}>
-          <View>
+          <View style={styles.sellerDetails}>
             <Text style={styles.bold}>{s.name}</Text>
             <Text>{s.address}</Text>
-            <Text>{s.state}, {s.pincode}</Text>
-            <Text>{s.gstin}</Text>
-            <Text>{s.pan}</Text>
+            <Text>
+              {s.state}, {s.pincode}
+            </Text>
+            <Text>GSTIN: {s.gstin}</Text>
+            <Text>PAN: {s.pan}</Text>
           </View>
           <View>
             <Text style={[styles.heading, styles.bold]}>TAX INVOICE</Text>
-            <Text>Invoice #: {invoice.invoiceNumber}</Text>
-            <Text>Date: {new Date(invoice.invoiceDate).toLocaleDateString()}</Text>
-            <Text>Order No: {invoice.buyerOrderNumber || "N/A"}</Text>
+            <View style={styles.invoiceDetails}>
+              <Text>Invoice #: {invoice.invoiceNumber}</Text>
+            <Text>
+              Date: {new Date(invoice.invoiceDate).toLocaleDateString()}
+            </Text>
+            </View>
+            <View style={styles.buyerOrderDetails}>
+              <Text>Order No: {invoice.buyerOrderNumber || "N/A"}</Text>
+            <Text>
+              Date: {new Date(invoice.buyerOrderDate).toLocaleDateString()}
+            </Text>
+            </View>
           </View>
         </View>
 
         {/* Bill To / Ship To */}
-        <View style={[styles.section, styles.row]}>
-          <View style={{ width: "48%" }}>
+        <View style={[styles.section, styles.row, styles.buyerSections]}>
+          <View style={styles.billToDetails}>
             <Text style={styles.bold}>Bill To:</Text>
             <Text>{b.name}</Text>
             <Text>{b.addressLine1}</Text>
-            <Text>{b.state}, {b.pincode}</Text>
+            <Text>
+              {b.state}, {b.pincode}
+            </Text>
             <Text>{b.phone}</Text>
             <Text>{b.email}</Text>
           </View>
-          <View style={{ width: "48%" }}>
+          <View style={styles.shipToDetails}>
             <Text style={styles.bold}>Ship To:</Text>
             <Text>{sh.name}</Text>
             <Text>{sh.addressLine1}</Text>
-            <Text>{sh.state}, {sh.pincode}</Text>
+            <Text>
+              {sh.state}, {sh.pincode}
+            </Text>
             <Text>{sh.phone}</Text>
             <Text>{sh.email}</Text>
           </View>
@@ -99,7 +289,7 @@ const InvoicePDF = ({ invoice }) => {
           </View>
           {products.map((item, i) => (
             <View style={styles.row} key={i}>
-              <Text style={{ width: "5%",paddingLeft:5 }}>{i + 1}</Text>
+              <Text style={{ width: "5%", paddingLeft: 5 }}>{i + 1}</Text>
               <Text style={{ width: "30%" }}>{item.description}</Text>
               <Text style={{ width: "10%" }}>{item.hsn}</Text>
               <Text style={{ width: "10%" }}>{item.quantity}</Text>
@@ -112,21 +302,14 @@ const InvoicePDF = ({ invoice }) => {
         </View>
 
         {/* Totals Section */}
-        <View style={[styles.section, { alignItems: "flex-end" }]}>
-          <Text>
-  Subtotal: ₹{subtotal.toLocaleString("en-IN", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
-</Text>
-<Text>
-  Tax ({taxRate}%): ₹{taxAmount.toLocaleString("en-IN", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
-</Text>
-<Text>
-  Grand Total: ₹{total.toLocaleString("en-IN", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
-</Text>
-        </View>
-
-        {/* Bank Details + Signatory */}
-        <View style={styles.row}>
-          <View style={{ width: "60%" }}>
+        <View style={[styles.section, styles.totalsSection]}>
+          <View>
+            <Text>Grand Total (in words)</Text>
+            <Text style={styles.totalInWords}>INR {toWords(total)} Only</Text>
+          </View>
+          <View style={styles.totalSection}>
+            {/* Bank Details + Signatory */}
+          <View>
             <Text style={styles.bold}>Bank Details:</Text>
             <Text>Bank: {s.bankName}</Text>
             <Text>Acc No: {s.accountNumber}</Text>
@@ -134,13 +317,45 @@ const InvoicePDF = ({ invoice }) => {
             <Text>IFSC: {s.ifsc}</Text>
             <Text>PAN: {s.pan}</Text>
           </View>
-          <View style={{ width: "35%", textAlign: "right" }}>
-            <Text style={[styles.bold, styles.authorisedSignatory]}>Authorised Signatory</Text>
+            <View style={styles.subTotalSection}>
+              <Text>
+              Subtotal: ₹
+              {subtotal.toLocaleString("en-IN", {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })}
+            </Text>
+            <Text>
+              Tax ({taxRate}%): ₹
+              {taxAmount.toLocaleString("en-IN", {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })}
+            </Text>
+            <Text>
+              Grand Total: ₹
+              {total.toLocaleString("en-IN", {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              })}
+            </Text>
+            </View>
           </View>
         </View>
 
+        
+        <View style={styles.authorisedSignatory}>
+            <Text style={[styles.bold]}>
+              Authorised Signatory
+            </Text>
+          </View>
         {/* Footer */}
-        <Text style={styles.footerText}>This is a Computer Generated Invoice</Text>
+        <Text style={styles.footerText}>
+          This is a Computer Generated Invoice
+        </Text>
+        <Text style={[styles.footerText, styles.Watermark]}>
+          Made by RTI Invoicer
+        </Text>
       </Page>
     </Document>
   );
